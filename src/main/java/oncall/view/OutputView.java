7b72cbb501b1a1efb7fa72577215c9a4123063cc
Min <binine2@gmail.com>
2024-12-12 16:57:23 +0900

    feat(OutputView): 마지막 결과 출력 메서드 구현

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -1,5 +1,7 @@
 package oncall.view;
 
+import java.util.List;
+
 public class OutputView {
     private static final String REQUEST_MONTH_AND_DAY = "비상 근무를 배정할 월과 시작 요일을 입력하세요> ";
     private static final String REQUEST_WEEKDAY_WORKER = "평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ";
@@ -16,5 +18,11 @@ public class OutputView {
 
     public static void requestHolidayWorker() {
         System.out.print(REQUEST_HOLIDAY_WORKER);
+    }
+
+    public static void printResult(List<String> results) {
+        for (String result : results) {
+            System.out.println(result);
+        }
     }
 }
